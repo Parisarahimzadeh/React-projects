1e993b898ef7f78a515a0d3a53667a5ca2814c77
Parisarahimzadeh <106354887+Parisarahimzadeh@users.noreply.github.com>
2023-12-14 11:19:18 +0330

    slidershow

---
+++ sliderShow/README.md
@@ -0,0 +1,2 @@
+a slider which is controlled by button in order or randomly
+created by React
---
+++ sliderShow/src/data.js
@@ -0,0 +1,29 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'پریسا رحیم زاده',
+        job: 'طراح وب',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ_SEpxJE_9PCTfFmp2RAx9rGL7BB56rJSh1sPSlMj4Q&s',
+        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+    
+    },
+    {
+        id: 2,
+        name: 'شایان شیخی',
+        job: 'گرافیست',
+        image: 'https://thumbs.dreamstime.com/b/portrait-young-african-american-business-woman-black-peop-people-51712509.jpg',
+        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است',
+    
+    },
+    {
+        id: 3,
+        name: 'نوشین فلاحی',
+        job: 'برنامه نویس',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyr5Y-xWDAxdkoC3W6j_Pw2SWuYUjFdobPaQ&usqp=CAU',
+        text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. ت',
+    
+    }
+]
+
+
+export default reviews;
